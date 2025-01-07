bb90d7f88aa63e3ac22dd42b8e3c8de7e8916fe3
David Barringer <78115882+DNBarringer@users.noreply.github.com>
2025-01-06 20:42:56 -0700

    Initial commit

---
+++ app/app.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+const { getImages } = useFile()
+
+useHead({
+  link: [
+    { rel: 'icon', href: '/favicon.ico' }
+  ],
+  htmlAttrs: {
+    lang: 'en'
+  }
+})
+
+const title = 'DB Photography'
+const description = 'A collection of photos taken by David Barringer'
+
+useSeoMeta({
+  title,
+  ogTitle: title,
+  description,
+  ogDescription: description,
+  ogSiteName: 'DB Photography',
+  ogImage: 'https://image-gallery.nuxt.dev/social-card.png',
+  twitterCard: 'summary_large_image'
+})
+
+await getImages()
+</script>
+
+<template>
+  <div
+    class="bg-white min-h-[100dvh] overflow-x-auto relative"
+    :class="{ 'flex flex-col md:block': $router.currentRoute.value.fullPath !== '/' }"
+  >
+    <UNotifications />
+    <NuxtPage />
+    <ImageThumbnailList :class="$router.currentRoute.value.fullPath !== '/' ? 'opacity-100 z-[9999]' : 'opacity-0 z-[-1]'" />
+  </div>
+</template>
